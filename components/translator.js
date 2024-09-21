3ca268e6f7f96f0ce3a476c9d066140e6625f40b
winningMove <dariopapic+github@gmail.com>
2024-09-21 18:15:49 +0200

    add all tests

--- components/translator.js
+++ components/translator.js
@@ -13,13 +13,16 @@ class Translator {
   #translateTime(token, from, to) {
     const i = token.indexOf(from);
     const translated = token.slice(0, i) + to + token.slice(i + 1);
+    if (translated.charAt(translated.length - 1) === ".") {
+      return this.#spanify(translated.slice(0, -1)) + ".";
+    }
     return this.#spanify(translated);
   }
 
   #translateAmToBr(text) {
     let idiomsTranslated = text;
     Object.entries(americanOnly).forEach(([k, v]) => {
-      const regex = new RegExp(k, "gi");
+      const regex = new RegExp(`\\b${k}\\b`, "gi");
       idiomsTranslated = idiomsTranslated.replace(regex, this.#spanify(v));
     });
 
@@ -53,14 +56,14 @@ class Translator {
   #translateBrToAm(text) {
     let idiomsTranslated = text;
     Object.entries(britishOnly).forEach(([k, v]) => {
-      const regex = new RegExp(k, "gi");
+      const regex = new RegExp(`(?<!fish-and-)\\b${k}\\b`, "gi");
       idiomsTranslated = idiomsTranslated.replace(regex, this.#spanify(v));
     });
 
     const timeRegex = /^\d{1,2}[.]\d{2}/;
     const translation = idiomsTranslated
       .split(" ")
-      .map((token) => {
+      .map((token, index) => {
         if (timeRegex.test(token)) {
           return this.#translateTime(token, ".", ":");
         }
@@ -93,6 +96,7 @@ class Translator {
       case "british-to-american":
         translation = this.#translateBrToAm(text);
     }
+    if (text === translation) return "Everything looks good to me!";
     return translation;
   }
 }
